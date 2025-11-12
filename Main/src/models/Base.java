dd786ae5d69b8cd95eda4995c85ba481469b3acb
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-11 21:30:01 -0300

    Se crea Base
    Se crea Clase Base para administrar los ID y eliminados; para saber su estado de las ID y eliminados en la base de datos

--- Main/src/models/Base.java
+++ Main/src/models/Base.java
@@ -4,10 +4,39 @@
  */
 package models;
 
-/**
- *
- * @author iRb18
- */
+
+// Este atributo Base es para heredar id y eliminados para todas las clases presentes del dominio, que son atributos comunes
 public abstract class Base {
+    private int id; // Identificado unico 
+    private Boolean eliminado; // Marca en nuestra base de datos un elemento fue eliminado
+    
+    // Se realiza el constructor sobrecargado
+    public Base(int id, Boolean eliminado){
+        this.id = id;
+        this.eliminado = eliminado;
+       
+    }
+    
+    public Base (){}  // constructor vacio
+    
+    
+    // Se hace el getters y setters 
+    public int getId(){
+        return id;
+    }
+    
+    public void setId (int id){
+        this.id = id;
+    }
+    
+    // Confirmación si esta eliminado el dato en la BD
+    public Boolean isEliminado(){
+        return eliminado;
+    }
+    
+    public void setEliminado (Boolean eliminado){
+        this.eliminado = eliminado;
+   }
     
 }
+    
