66a1ef05b3834df2a5b4b531a57cb980e655fa29
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-13 02:28:15 -0300

    Integración de entities + Gettes y Setters de Mascotas
    Se realiza la integración de los demas branch  Base, Mascotas y Michochip, y se completa los getters y setters de los atributos de microchip ademas del tostring. Se acuerda como grupo en vez de divirdir Branch hacerlo por tareas.

---
+++ Main/src/entities/Mascota.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Federico
+ */
+
+public class Mascota extends Base {
+
+    private String nombre;
+    private String especie;
+    private String raza;
+    private java.time.LocalDate fechaNacimiento;
+    private String duenio;
+    private Microchip microchip;
+
+    public Mascota() {
+    }
+    // Se realiza el constructor sobrecargado  
+    
+    public Mascota (Long id, Boolean eliminado, String nombre, String especie,String raza, LocalDate fechaNacimiento, String duenio, Microchip microchip){
+        super(id, eliminado);
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this. fechaNacimiento = fechaNacimiento; 
+        this.duenio = duenio;
+        this.microchip = microchip;
+    
+    }
+    // Sealizan todos los getters y setters de todos los atributos
+    
+    public String getNombre() {
+     return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getEspecie() {
+     return especie;
+    }
+    
+    public void setEspecie(String especie){
+        this.especie = especie;
+    }    
+    
+    public String getRaza() {
+     return raza;
+    }
+    
+    public void setRaza(String raza){
+        this.raza = raza;
+    }      
+    
+     public java.time.LocalDate getFechaNacimiento() {
+     return fechaNacimiento;
+    }
+    
+    public void setFechaNacimiento (java.time.LocalDate fechaNacimiento){
+        this.fechaNacimiento = fechaNacimiento;
+    }   
+  
+    public String getDuenio() {
+     return duenio;
+    }
+    
+    public void setDuenio(String duenio){
+        this.duenio = duenio;
+    }
+    
+    public Microchip getMicrochip() {
+        return microchip;
+    }
+    
+    public void setMicrochip(Microchip microchip){
+        this.microchip = microchip;
+    }
+    
+   @Override
+   public String toString(){
+       return "Mascota{" +
+               "id=" + super.getId() + 
+               ", eliminado'" + super.getEliminado() + 
+               ", nombre='" + nombre + '\'' + 
+               ", especie='" + especie + '\'' +
+               ", raza='" + raza + '\'' + 
+               ", duenio='" + duenio +'\'' +
+               ", microchip=" + (microchip != null ? microchip.getCodigo() : "Sin Chip") + 
+               '}';        
+   }
+    
+}
---
+++ Main/src/entities/Microchip.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+/**
+ *
+ * @author iRb18
+ */
+import java.time.LocalDate;
+
+public class Microchip extends Base {
+    private String codigo;
+    private LocalDate fechaImplantacion;
+    private String veterinaria;
+    private String observaciones;
+
+    public Microchip() {}
+    
+    public Microchip(Long id, Boolean eliminado, String codigo, LocalDate fechaImplantacion, String veterinaria, String observaciones) {
+        super(id, eliminado);
+        this.codigo = codigo;
+        this.fechaImplantacion = fechaImplantacion;
+        this.veterinaria = veterinaria;
+        this.observaciones = observaciones;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getFechaImplantacion() {
+        return fechaImplantacion;
+    }
+    public void setFechaImplantacion(LocalDate fechaImplantacion) {
+        this.fechaImplantacion = fechaImplantacion;
+    }
+
+    public String getVeterinaria() {
+        return veterinaria;
+    }
+    public void setVeterinaria(String veterinaria) {
+        this.veterinaria = veterinaria;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Microchip{" +
+                "id=" + super.getId() +
+                ", eliminado=" + super.getEliminado() +
+                ", codigo='" + codigo + '\'' +
+                ", fechaImplantacion=" + fechaImplantacion +
+                ", veterinaria='" + veterinaria + '\'' +
+                ", observaciones='" + observaciones + '\'' +
+                '}';
+    }
+}
