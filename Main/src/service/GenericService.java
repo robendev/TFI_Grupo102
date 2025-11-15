df11f687f620b4c30346ae3dde47daa3e31a4b11
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-15 16:47:11 -0300

    Se remueve CRUD metodo de GenericService interface
    Se eliminaron las firmas de los métodos de operación CRUD de la interfaz GenericService. Para enfocarse en DAO en esta rama

--- Main/src/service/GenericService.java
+++ Main/src/service/GenericService.java
@@ -8,17 +8,7 @@
  */
 package service;
 
-import java.util.List;
 
 public interface GenericService<T> {
-// insterta una nueva entidad en el sistema
-    void crear(T entidad) throws Exception;
-//Identificador de la entidad por su ID
-    T leer(int id) throws Exception;
-//Retorna todas las entidades del sistema
-    List<T> leerTodos() throws Exception;
-//Actualiza una entidad existente
-    void actualizar(T entidad) throws Exception;
-//Elimina una entidad por su ID
-    void eliminar(int id) throws Exception;
+
 }
