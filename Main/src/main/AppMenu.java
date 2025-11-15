f82bd44be8ba3424980d45587f4924e4a40f393e
Victormanuel.bar <victormanuel.bar28@gmail.com>
2025-11-15 16:44:14 -0300

    Update AppMenu.java
    Se remueve Main class para que no haya problemas de implementación con otras ramas, se enfocara en corrección DAO

--- Main/src/main/AppMenu.java
+++ Main/src/main/AppMenu.java
@@ -4,19 +4,6 @@
  */
 package main;
 
-import dao.MascotaDao;
-import java.util.Scanner;
-import entities.Base;
-import entities.Mascota;
-
-import service.MascotaService;
-import service.MicrochipService;
-import dao.MascotaDao;
-import dao.MicrochipDao;
-
-import java.util.Scanner;
-import java.util.List;
-
 
 /**
  *
@@ -24,30 +11,5 @@ import java.util.List;
  */
 public class AppMenu {
 
-
-        
-public class AppMenu {
-     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         
-         MascotaService mascotaService = new MascotaService(new MascotaDao());
-        // MicrochipService microchipService = new MicrochipService(new MicrochipDao(),mascota);
-          int opcion = -1;
-          do {
-              try {
-                  menu();
-                  opcion = Integer.parseInt(sc.nextLine());
-                  
-                  switch(opcion){
-                      case 1 -> {
-                          try {
-                              Mascota mascota = new Mascota();
-                              System.out.print( "")
-                          }
-                      }
-              }
-          }
-     }
-}
 
 }
